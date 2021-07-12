e8ce557156db21aa8b2d52aaec4d7e2e5c9740bb
DeepakM2001 <51810249+DeepakM2001@users.noreply.github.com>
2021-07-12 08:51:17 +0530

    Add files via upload

---
+++ d26/Ex1.py
@@ -0,0 +1,4 @@
+
+list1= ['deepak','kumar','babu','ram']
+print(list(enumerate(list1)))
+print(enumerate(list1))
---
+++ d26/Ex2.py
@@ -0,0 +1,3 @@
+
+tuple_deepak_mgl=('deepak','ram','raj','balu')
+print(tuple(enumerate(tuple_deepak_mgl)))
